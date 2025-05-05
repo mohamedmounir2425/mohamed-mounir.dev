76ee23637531798cb9d2452d36962d63bfc606cd
Mohamed Mounir <mohamed.mounir2425@gmail.com>
2025-05-05 10:38:00 +0300

    👨‍💻 exerience

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/layout/Header";
 import "./assets/css/index.css";
 import Hero from "./pages/Hero/Hero";
 import SkillsSection from "./pages/Skills/Skills";
+import ExperienceSection from "./pages/Experience/Experience";
 export default function App() {
     return (
         <>
             <Header />
             <Hero />
             <SkillsSection />
+            <ExperienceSection />
         </>
     );
 }
